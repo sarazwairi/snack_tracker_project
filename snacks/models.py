f50ed3d39cbdd05299cc486c4fb8777b1879f7e3
Sara Zwairi <sara.zwairi@gmail.com>
2021-08-30 23:39:47 +0300

    lab 27 done

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+class Snacks(models.Model):
+    name=models.CharField(max_length=64)
+    purchaser=models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    description=models.TextField(max_length=255,default=0)
+
+    def __str__(self):
+            return self.name
--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from .models import Snacks
+
 
 # Create your views here.
+class SnackListView(ListView):
+    template_name='snack_list.html'
+    model=Snacks
+
+class SnackDetailView(DetailView):
+    template_name='snack_detail.html'
+    model=Snacks
